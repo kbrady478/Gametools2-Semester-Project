a4a9eb01c50a52873f10e2018bd6447721aad942
kb478 <c23789389@mytudublin.ie>
2025-01-08 18:32:36 +0000

    beta?

---
+++ Gametools2-Semester-Project/Assets/! Scripts/Menu UI/Menu_Start.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem.UI;
+
+public class Menu_Start : MonoBehaviour
+{
+
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
